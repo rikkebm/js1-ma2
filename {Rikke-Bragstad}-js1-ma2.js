379d3507df020beb50b838d0bf53b3dafc9bed4e
rikkebm <60385097+rikkebm@users.noreply.github.com>
2020-04-05 16:02:33 +0200

    Create {Rikke-Bragstad}-js1-ma2.js

---
+++ {Rikke-Bragstad}-js1-ma2.js
@@ -0,0 +1,89 @@
+//Question 1
+
+//var myFunctionExpression = function();
+//console.log("Rikke");
+//myFunctionExpression();
+
+//Question 2
+
+//const button = document.querySelector(".btn");
+//function buttonClicked(){
+//  console.log("I was clicked");
+//};
+//button.addEventListener("click", buttonClicked);
+
+//Question 3
+
+//document.getElementById("firstname").addEventListener("keydown", callAfterKeyIsPressed);
+//function callAfterKeyIsPressed(e){
+//    console.log(e.keyCode);
+//}
+
+//Question 4
+
+//var dogButton = document.getElementsByTagName("button")[0]
+//dogButton.addEventListener("mouseover", () => {dogButton.classList.add("hover")})
+
+//Question 5
+
+//var dogButton = document.querySelector("[data-animal='dog']");
+//dogButton.addEventListener("mouseout", () => {dogButton.classList.remove("hover")})
+
+//Question 6
+
+//var listedElements = document.querySelectorAll("li");
+//for (let i = 0; i < listedElements.length; i++) {
+//}
+//listedElements[i].addEventListener("mouseover", callAfter);
+//function callAfter(e){
+//console.log("[data-animal=[i]]");
+//}
+
+//Question 7
+
+//switch (animal) {
+
+//    case "cat":
+//        console.log("Meow");
+//        break;
+
+//    case "cow":
+//        console.log("Moo");
+//        break;
+
+//    case "bird":
+//        console.log("Tweet");
+//        break;
+
+//    default:
+//        console.log("Harrumph");
+//}
+
+//Question 8
+
+//function handleEachSheep(item) {
+//    console.log(item);
+//}
+//sheep.forEach(handleEachSheep)
+
+//Question 9
+
+//function logTime() {
+//    console.log("hello");
+//}
+//    if(counter === 5) {
+//        clearInterval(intervalId);
+//    }
+//    counter++;
+//    }
+//let counter = 0;
+//const intervalId = setInterval(logTime, 500);
+
+
+//Question 10
+
+//var textDiv = document.getElementsByTagName("div")[0];
+//function updateDiv() {
+//    textDiv.innerHTML = "Text updated"
+//}
+//setTimeout(updateDiv, 200);
